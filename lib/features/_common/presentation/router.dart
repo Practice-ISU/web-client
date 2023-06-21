ddc975303cada8de91131d6ee437020db48c26c9
Lazarev Daniil <daniil.kn2004@gmail.com>
2023-06-21 11:43:27 +0800

    Add login page to router

--- lib/features/_common/presentation/router.dart
+++ lib/features/_common/presentation/router.dart
@@ -1,14 +1,13 @@
 import 'package:go_router/go_router.dart';
 import 'package:image_hoster/features/_common/presentation/navigation.dart';
+import 'package:image_hoster/features/auth/presentation/login/login_screen.dart';
 
 GoRouter createAppRouter() => GoRouter(
       initialLocation: Routes.login,
       routes: [
         GoRoute(
           path: Routes.login,
-        ),
-        GoRoute(
-          path: Routes.register,
+          builder: (context, state) => const LoginScreen()
         ),
       ],
     );
