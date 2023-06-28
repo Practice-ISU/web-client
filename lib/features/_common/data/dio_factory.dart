4eb045f6e2bc986ea8efca4d3e3bba1f5ee2c970
Lazarev Daniil <daniil.kn2004@gmail.com>
2023-06-29 00:39:50 +0800

    Add auth token error handler

--- lib/features/_common/data/dio_factory.dart
+++ lib/features/_common/data/dio_factory.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:image_hoster/features/_common/data/session_store.dart';
 import 'package:injectable/injectable.dart';
 
 @Singleton()
@@ -14,6 +15,24 @@ class DioFactory {
       receiveTimeout: _timeout,
     );
     final dio = Dio(options);
+
+    dio.interceptors.add(QueuedInterceptorsWrapper(
+        onResponse: (response, handler) async {
+          if (response.data['isSuccess'] == false && response.data['message'] == 'Invalid token') {
+            await SessionStore.instance.deleteAccessToken();
+            return handler.resolve(response);
+          }
+          return handler.next(response);
+        },
+        onError: (e, handler) async {
+          if (e.response?.statusCode == 401 || e.response?.statusCode == 403) {
+            await SessionStore.instance.deleteAccessToken();
+            return handler.reject(e);
+          }
+          return handler.next(e);
+        },
+      ),
+    );
 
     if (kDebugMode) {
       dio.interceptors.add(
